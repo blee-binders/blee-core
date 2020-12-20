5d6cefd5e98048e8d23ebab0a4286c070558b04c
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-12-20 10:03:34 -0800

    dblock updates.

--- ftoProc.sh
+++ ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/blee-core/ftoProc.sh 
+*  /This File/ :: /bisos/panels/blee/core/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
